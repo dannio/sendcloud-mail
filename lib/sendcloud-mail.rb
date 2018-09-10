3648d75a6c162482d30b18f83545818c674b3f6b
Dan Wong <dan.wong@o2-v2.com>
2018-09-10 14:46:35 +0800

    initial commit

---
+++ lib/sendcloud-mail.rb
@@ -0,0 +1,112 @@
+require 'digest'
+require 'rest-client'
+require 'json'
+require 'yaml'
+
+module SendCloud
+  class Mail
+
+    def self.load!(file, environment)
+      config = YAML.load_file(file)
+      self.auth(config[environment.to_s]['mail_api_user'], config[environment.to_s]['mail_api_key'])
+    end
+
+    def self.auth(mail_api_user, mail_api_key)
+      @mail_api_user = mail_api_user
+      @mail_api_key = mail_api_key
+    end
+
+    def self.send(from, fromName, subject, xsmtpapi, content, summary=nil)
+      # xsmtpapi comes in the following format:
+      #{
+      #  "to": ["d@163.com",'i@163.com'],
+      #  "sub": {
+      #      "%content%": ['nihao0', 'nihao1']
+      #  }
+      #}
+      response = RestClient.post 'https://api.sendcloud.net/apiv2/mail/send?',
+                                 apiUser: @mail_api_user,
+                                 apiKey: @mail_api_key,
+                                 from: from,
+                                 fromName: fromName,
+                                 xsmtpapi: xsmtpapi.to_json,
+                                 subject:, subject,
+                                 html: content,
+                                 contentSummary: summary
+      JSON.parse(response.to_s)['statusCode']
+    end
+
+    def self.send_template(invokeName, from, fromName, subject, xsmtpapi, summary=nil)
+      # xsmtpapi comes in the following format:
+      #{
+      #  "to": ["d@163.com",'i@163.com'],
+      #  "sub": {
+      #      "%content%": ['nihao0', 'nihao1']
+      #  }
+      #}
+      response = RestClient.post 'https://api.sendcloud.net/apiv2/mail/sendtemplate?',
+                                 apiUser: @mail_api_user,
+                                 apiKey: @mail_api_key,
+                                 from: from,
+                                 fromName: fromName,
+                                 xsmtpapi: xsmtpapi.to_json,
+                                 subject:, subject,
+                                 templateInvokeName: invokeName,
+                                 contentSummary: summary
+      JSON.parse(response.to_s)['statusCode']
+    end
+
+    def self.get(invokeName)
+      params = {
+        apiUser: @mail_api_user,
+        apiKey: @mail_api_key,
+        invokeName: invokeName,
+      }
+      response = RestClient.get 'https://api.sendcloud.net/apiv2/template/get?', params: params
+      JSON.parse(response.to_s)
+    end
+
+    def self.list
+      params = {
+        apiUser: @mail_api_user,
+        apiKey: @mail_api_key
+      }
+      response = RestClient.get 'https://api.sendcloud.net/apiv2/template/list?', params: params
+      JSON.parse(response.to_s)
+    end
+
+    def self.create(invokeName, name, html, subject, templateType)
+      response = RestClient.post 'http://api.sendcloud.net/apiv2/template/add?',
+                                  apiUser: @mail_api_user,
+                                  apiKey: @mail_api_key,
+                                  invokeName: invokeName,
+                                  name: name,
+                                  html: content,
+                                  subject: subject,
+                                  templateType: type
+
+      JSON.parse(response.to_s)
+    end
+
+    def self.update(invokeName, name, html, subject, templateType)
+      response = RestClient.post 'https://api.sendcloud.net/apiv2/template/update?',
+                                  apiUser: @mail_api_user,
+                                  apiKey: @mail_api_key,
+                                  invokeName: invokeName,
+                                  name: name,
+                                  html: content,
+                                  subject: subject,
+                                  templateType: type
+      JSON.parse(response.to_s)['statusCode']
+    end
+
+    def self.delete(invokeName)
+      response = RestClient.post 'https://api.sendcloud.net/apiv2/template/delete?',
+                                  apiUser: @mail_api_user,
+                                  apiKey: @mail_api_key,
+                                  invokeName: invokeName
+      JSON.parse(response.to_s)['statusCode']
+    end
+
+  end
+end
---
+++ sendcloud-mail.gemspec
@@ -0,0 +1,15 @@
+Gem::Specification.new do |s|
+  s.name        = 'sendcloud-mail'
+  s.version     = '0.0.1'
+  s.date        = '2018-09-03'
+  s.summary     = 'A gem to send Mail with SendCloud API'
+  s.description = 'A gem to send Mail with SendCloud API'
+  s.authors     = ['dannio']
+  s.email       = 'dcwongy@gmail.com'
+  s.files       = ['lib/sendcloud-mail.rb']
+  s.require_paths = ['lib']
+  s.homepage    = 'https://github.com/dannio/sendcloud-mail'
+  s.license     = 'MIT'
+
+  s.add_runtime_dependency 'rest-client', '>= 1.8.0'
+end
